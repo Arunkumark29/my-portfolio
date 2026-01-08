b1b29ccb4a865cf585db9db227142bff010b3482
Arunkumark29 <arunkumark.cse2023@citchennai.net.com>
2026-01-08 20:47:29 +0530

    Fix CSS import case issue

--- src/sections/Home/HomeSection.jsx
+++ src/sections/Home/HomeSection.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './home.css';
+import './Home.css';
 
 const HomeSection = ({ onStart }) => {
   const [isAssembled, setIsAssembled] = useState(false);
